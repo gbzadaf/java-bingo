0cf5844a612d0515b0f8aff1ab9f966abf682cc5
gbzadaf <fonsecagb23@gmail.com>
2026-09-06 21:41:14 -0300

    feat: comentarios

--- src/main/java/Bingo.java
+++ src/main/java/Bingo.java
@@ -8,11 +8,13 @@ public class Bingo {
     public static void main(String[] args) {
         Scanner scanner =  new Scanner(System.in);
 
+        //lista representando uma sequencia de numeros de 1 até 75
         List<Integer> bingo = new ArrayList<>();
         for (int i = 1; i <= 75; i++) {
             bingo.add(i);
         }
 
+        //efeito de "sortear" o codigo, pra nao precisa complicar o codigo, embaralha aqui e percorre a lista normal
         Collections.shuffle(bingo);
 
         for (int numero : bingo) {
@@ -30,6 +32,8 @@ public class Bingo {
         scanner.close();
     }
 
+
+    //toLowerCase para que transforme 'S' e 'N' maiusculo pra minusculo
     private static String perguntar (Scanner scanner, int numero) {
         System.out.println("O numero sorteado é: " + numero + ". Deseja continuar? (s/n): ");
         return scanner.nextLine().toLowerCase();
